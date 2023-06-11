1a367e4935ad1f48807acd178490292aa21da33f
Jeremy Sobczak <jeremy@jeremys-mbp.lan>
2023-06-11 11:40:37 -0400

    Initial commit. App skeleton set up with notes.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App - this component should render the BoxList component.
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
